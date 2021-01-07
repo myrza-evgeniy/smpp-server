b957eabec86927b3437a66f5e4f1ead4d22a7fef
Mirza Eugeniu <eugeniu.mirza@dassolutions.eu>
2021-01-07 20:22:05 +0200

    Initialize new spring boot project

---
+++ src/main/java/com/opensource/smppserver/SmppServerApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.smppserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmppServerApplication.class, args);
+	}
+
+}
